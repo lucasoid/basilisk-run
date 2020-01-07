552df7a4886f5bdc4578a082445a82235e37d43e
Ben Lucas <ben.david.lucas@gmail.com>
2020-01-06 23:14:50 -0500

    added mute button

--- src/index.js
+++ src/index.js
@@ -2,3 +2,27 @@ import { mountGame } from './game';
 
 const el = document.getElementById('root');
 const game = mountGame(el);
+
+// controls
+
+function configureMuteButton() {
+    const muteButton = document.getElementById('toggle-mute');
+
+    function setMuteButton(isMuted) {
+        muteButton.innerHTML = isMuted
+            ? '<span class="fas fa-volume-mute"></span>'
+            : '<span class="fas fa-volume-up"></span>';
+        muteButton.title = isMuted ? 'Unmute' : 'Mute';
+        muteButton.className = isMuted ? 'muted' : 'unmuted';
+    }
+
+    setMuteButton(true);
+
+    muteButton.addEventListener('click', function(evt) {
+        const currentValue = game.sound.mute;
+        game.sound.setMute(!currentValue);
+        setMuteButton(!currentValue);
+    });
+}
+
+configureMuteButton();
