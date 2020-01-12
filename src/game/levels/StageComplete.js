20c548df2d21799a5c18a8914cabb70d2bd27a21
Ben Lucas <ben.david.lucas@gmail.com>
2020-01-11 22:47:16 -0500

    updated transition view

--- src/game/levels/StageComplete.js
+++ src/game/levels/StageComplete.js
@@ -1,17 +1,28 @@
 import { Scene } from 'phaser';
-import { Basilisk } from '../models/Basilisk';
 import constants from '../constants';
+import bgImg from '../assets/forest-aerial.png';
 
 export class StageComplete extends Scene {
     level;
     transitionTo;
 
     init = data => {
-        this.level = data.level;
-        this.transitionTo = data.transitionTo;
+        this.level = (data && data.level) || '';
+        this.transitionTo = (data && data.transitionTo) || null;
+    };
+
+    preload = () => {
+        if (!this.textures.exists('transitionBg'))
+            this.load.image('transitionBg', bgImg);
     };
 
     create = () => {
+        const background = this.add.image(
+            constants.screen.WIDTH / 2,
+            constants.screen.HEIGHT / 2,
+            'transitionBg'
+        );
+        background.setScrollFactor(0);
         const textWidth = 300;
         const text = this.add.text(
             constants.screen.WIDTH / 2 - textWidth / 2,
@@ -25,24 +36,9 @@ export class StageComplete extends Scene {
         );
         text.setAlign('center');
 
-        // lizard spin
-        let basiliskSprite = this.physics.add.sprite(
-            constants.screen.WIDTH / 2,
-            constants.screen.HEIGHT / 2,
-            Basilisk.handle
-        );
-        let i = 0;
-        let interval = setInterval(() => {
-            basiliskSprite.setAngle(i * 30).setScale(1 / (1 + i * 0.1));
-            if (i * 30 >= 720) {
-                clearInterval(interval);
-                basiliskSprite.destroy();
-                setTimeout(() => {
-                    this.scene.stop();
-                    this.scene.start(this.transitionTo);
-                }, 2000);
-            }
-            i++;
-        }, 25);
+        setTimeout(() => {
+            this.scene.stop();
+            this.scene.start(this.transitionTo);
+        }, 2000);
     };
 }
